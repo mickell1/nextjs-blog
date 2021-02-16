82e6249d12a7524fe24c4f51d507d466fc57f24c
Mickell <mickellc@hotmail.co.uk>
2021-02-16 10:53:08 +0000

    Add nextjs files

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/Date";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm a full stack JavaScript engineer & consultant. I use Agile methods
+          to help build digital products, teams and systems. I have 8 years
+          experience working in the technology sector, this includes: software
+          development, consultancy, finance, hospitality, field engineer and
+          tech support. Also Professional Scrum master certified(PSM1). I
+          currently work as a frontend developer and mainly work with
+        </p>
+        <p className={utilStyles.boldDescription}>
+          React, JavaScript, CSS/SASS, TypeScript and Jest.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
